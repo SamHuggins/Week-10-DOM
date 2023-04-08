60052c405ff40a7a6d9bfe9500021aefdef70c8f
sam <sam.jacie.huggins@gmail.com>
2023-04-08 16:56:14 -0500

    Improved User Experience

--- scripts.js
+++ scripts.js
@@ -7,5 +7,8 @@ document.getElementById('submit').addEventListener('click', () => {
     row.insertCell(0).innerHTML = document.getElementById('new-first-name').value;
     row.insertCell(1).innerHTML = document.getElementById('new-last-name').value; 
     row.insertCell(2).innerHTML = document.getElementById('new-pronoun').value; 
+    document.getElementById('new-first-name').value = '';
+    document.getElementById('new-last-name').value = '';
+    document.getElementById('new-first-name').value = 'Pronouns';
 });
 
