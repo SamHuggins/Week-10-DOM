88d2148c647160921844992fe8b84cf2de66a52d
sam <sam.jacie.huggins@gmail.com>
2023-04-08 16:25:37 -0500

    final commt

---
+++ scripts.js
@@ -0,0 +1,11 @@
+let id = 0;
+
+document.getElementById('submit').addEventListener('click', () => {
+    let table = document.getElementById('list');
+    let row = table.insertRow(1);
+    row.setAttribute('id', `item-${id}`);
+    row.insertCell(0).innerHTML = document.getElementById('new-first-name').value;
+    row.insertCell(1).innerHTML = document.getElementById('new-last-name').value; 
+    row.insertCell(2).innerHTML = document.getElementById('new-pronoun').value; 
+});
+
